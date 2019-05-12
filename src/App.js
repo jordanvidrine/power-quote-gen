e039eb82b2a04807ce2f78e46d36439747fb1f77
Jordan Vidrine <jordan@jordanvidrine.com>
2019-05-11 20:37:24 -0500

    CustomerInfo component and its related state + validation

--- src/App.js
+++ src/App.js
@@ -2,19 +2,114 @@ import React, {Component} from 'react';
 import CustomerInfo from './CustomerInfo'
 import './App.css';
 
+var validator = require("email-validator");
+
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {
+      customerInfo: {
+        projectTitle: '',
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: '',
+        address: '',
+        state: 'LA',
+        zip: '',
+        quoteType: '',
+        needsToBeCorrected: []
+      },
+    }
+
+  this.handleChange = this.handleChange.bind(this)
+  this.formValidator = this.formValidator.bind(this)
+  }
+
+  handleChange(e) {
+    let info = e.target.name;
+    this.setState({
+      ...this.state,
+      customerInfo : {
+        ...this.state.customerInfo,
+        [info]: e.target.value,
+      }
+    })
+  }
+
+  emailTester() {
+    let emailToTest = document.getElementById("emailTest")
+    console.log(emailToTest.value)
+    if (validator.validate(emailToTest.value)) {
+      console.log('Email is good')
+    } else {
+      console.log('Email is bad')
+    }
+  }
+
+  formValidator(){
+
+    let customerValidationObject = {}
+    for (let info in this.state.customerInfo) {
+      if (info === 'email') {
+        customerValidationObject[info] = validator.validate(this.state.customerInfo[info])
+      } else if (info === 'isValid') {
+        } else {
+        customerValidationObject[info] = (this.state.customerInfo[info] !== '')
+        }
+      }
+
+    let needsToBeCorrected = [];
+
+    for (let key in customerValidationObject) {
+      if (!customerValidationObject[key]) {
+        needsToBeCorrected.push(key)
+      }
+    }
+    if (needsToBeCorrected.length === 0) {
+      this.setState({
+        ...this.state,
+        customerInfo: {
+          ...this.state.customerInfo,
+          needsToBeCorrected,
+          isValid: true,
+        }})
+      } else if (needsToBeCorrected.length !== 0) {
+        this.setState({
+          ...this.state,
+          customerInfo: {
+            ...this.state.customerInfo,
+            needsToBeCorrected,
+            isValid: false,
+          }})
+      }
   }
 
   render() {
 
+  var warningError;
+  if (this.state.customerInfo.isValid === false) {
+    warningError = this.state.customerInfo.needsToBeCorrected.reduce((acc,cur,idx,array) => {
+    if (idx < array.length-1) {
+        acc = acc + '' + ' ' + cur + ',';return acc
+    } else {
+      return acc + ' ' + cur + '.' + ' Once corrected, try submitting again.';
+    }},'The following items need to be corrected:');
+  } else {
+    warningError = '';
+  }
 
   return (
     <div className="App">
     <h1>Quote Generation App</h1>
-    <CustomerInfo />
+    <CustomerInfo
+      handleChange={this.handleChange}
+      customerInfo={this.state.customerInfo}
+      formValidator={this.formValidator}
+    />
+    <p className='warning'>
+      {warningError}
+    </p>
     </div>
   );
   }
--- src/CustomerInfo.js
+++ src/CustomerInfo.js
@@ -8,73 +8,103 @@ class CustomerInfo extends Component {
   render() {
     return(
       <div id='CustomerForm'>
-        <h3>Customer Information</h3>
-        <label for="projectTitle">Project Title</label><input type='text' name="projectTitle"></input>
-        <div>
-          <label for="firstName">First Name</label><input type='text' name="firstName"></input>
-          <label for="lastName">Last Name</label><input type='text' name="lastName"></input>
+      <h3>Customer Information</h3>
+      <div id="projectTitle">
+        <label>Project Title</label>
+        <input
+          onChange={this.props.handleChange}
+          type='text'
+          name="projectTitle"
+          value={this.props.customerInfo.projectTitle}
+        ></input>
+      </div>
+
+        <div id="customerInfo">
+          <div id="customerName">
+            <label>First Name</label>
+            <input
+              onChange={this.props.handleChange}
+              type='text'
+              name="firstName"
+              value={this.props.customerInfo.firstName}
+            ></input>
+            <label>Last Name</label>
+            <input
+              onChange={this.props.handleChange}
+              type='text'
+              name="lastName"
+              value={this.props.customerInfo.lastName}
+            ></input>
+          </div>
+          <div id="customerContact">
+            <label>Phone Number</label>
+            <input
+              onChange={this.props.handleChange}
+              type='text'
+              name="phone"
+              value={this.props.customerInfo.phone}
+            ></input>
+            <label>Email Address</label>
+            <input
+              onChange={this.props.handleChange}
+              type='text'
+              name="email"
+              value={this.props.customerInfo.email}
+            ></input>
+          </div>
         </div>
-        <div>
-          <label for="phone">Phone Number</label><input type='text' name="phone"></input>
-          <label for="email">Email Address</label><input type='text' name="email"></input>
+
+        <div id="customerLocation">
+          <label>Address</label>
+          <input
+            onChange={this.props.handleChange}
+            type='text'
+            name="address"
+            value={this.props.customerInfo.address}
+          ></input>
+          <label>State</label>
+            <select
+              onChange={this.props.handleChange}
+              name="state">
+              <option value="AL">Alabama</option>
+              <option value="AR">Arkansas</option>
+              <option selected="selected" value="LA">Louisiana</option>
+              <option value="MS">Mississippi</option>
+              <option value="TX">Texas</option>
+            </select>
+          <label>Zip</label>
+          <input
+            onChange={this.props.handleChange}
+            type='text'
+            name="zip"
+            value={this.props.customerInfo.zip}
+          ></input>
         </div>
-        <label for="address">Address</label><input type='text' name="address"></input>
-        <label for="state">State</label>
-          <select name="state">
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District Of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </select>
-        <label for="zipCode">Zip</label><input type='text' name="zipCode"></input>
-        <input type="radio" name="quoteType" value="genInstall"/>Generator Install
+
+        <div id="typeOfWork">
+          <input onChange={this.props.handleChange}
+            type="radio"
+            name="quoteType"
+            value="genInstall"
+          />
+          <label>Gen Install</label>
+          <input
+            onChange={this.props.handleChange}
+            type="radio"
+            name="quoteType"
+            value="genMaint"
+          />
+          <label>Gen Maint</label>
+          <input
+            onChange={this.props.handleChange}
+            type="radio"
+            name="quoteType"
+            value="electrical"
+          />
+            <label>Elec Const</label>
+        </div>
+        <button id="next" onClick={this.props.formValidator}>Next</button>
+
       </div>
     )
   }
